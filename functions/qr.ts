4a36248bf28d6c64f2d34cafbd5e5bfb03b89166
Hugo Dias <hugomrdias@gmail.com>
2024-02-23 17:51:57 +0000

    feat: support template and mime types (#4)

--- functions/qr.ts
+++ functions/qr.ts
@@ -26,3 +26,20 @@ export function qrpng(data: string, output: string): string {
 export function concat(a: string, b: string): string {
   return a + b
 }
+
+/**
+ *
+ * @param a
+ * @param b
+ */
+export function add(a: number, b: number): number {
+  return a + b
+}
+
+/**
+ *
+ * @param val
+ */
+export function arrify(val: number): number[] {
+  return Array.isArray(val) ? val : [val]
+}
