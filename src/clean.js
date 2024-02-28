ef05125eecae7d5d9fe9f4da5d2b91e39147c087
Andrew Vivash <andrewvivash@Andrews-MBP.lan>
2024-02-28 11:58:25 -0800

    Fix: check if config dir has already been removed

--- src/clean.js
+++ src/clean.js
@@ -6,7 +6,11 @@ import { CONFIG_PATH } from '../cli.js'
 export async function clean() {
   const spinner = ora('Removing working directory').start()
 
-  await fs.rm(CONFIG_PATH, { recursive: true })
-
-  spinner.succeed('Working directory removed')
+  try {
+    await fs.access(CONFIG_PATH)
+    await fs.rm(CONFIG_PATH, { recursive: true })
+    spinner.succeed('Working directory removed')
+  } catch (error) {
+    spinner.succeed('Working directory already removed')
+  }
 }
